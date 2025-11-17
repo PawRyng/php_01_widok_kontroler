71e928f9846f0ba254c299fc2da1b120ae87de5a
PawRyng <p_ryng@wp.pl>
2025-11-17 18:58:17 +0100

    feat: add initial setup for Twig templating, Dockerfile for Composer, and update credit view

--- app/views/credit/credit_view.php
+++ app/views/credit/credit_view.php
@@ -1,30 +1,51 @@
-<!DOCTYPE HTML>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
-<head>
-<meta charset="utf-8" />
-<title>Kalkulator</title>
-</head>
-<body>
-	<?php require_once dirname(__FILE__).'/../../component/menu.php'; ?>
-<form action="/app/views/credit/calc.php" method="post">
-	<label for="id_x">Kwota: </label>
-	<input id="id_x" type="number" name="amount" value="<?= $amount ?? '' ?>" /><br />
-	<label for="id_x">Lata: </label>
-	<input id="id_x" type="number" name="years" value="<?= $years ?? '' ?>" /><br />
+<?php
+// Ładujemy centralny config (może zainicjować $twig)
+require_once dirname(__DIR__, 3) . '/config.php';
 
-	<label for="id_op">Oprocentowanie: </label>
-	<input id="id_op" type="text" name="interest"
-		inputmode="decimal"
-		pattern="[0-9]+([.,][0-9]+)?"
-		title="Użyj przecinka lub kropki jako separatora dziesiętnego"
-		value="<?= htmlspecialchars($interest ?? '', ENT_QUOTES) ?>" /><br />
-	<input type="submit" value="Oblicz" />
-</form>	
+if (isset($twig) && $twig instanceof \Twig\Environment) {
+	ob_start();
+	require_once dirname(__FILE__).'/../../component/menu.php';
+	$menu = ob_get_clean();
 
-<?php if (isset($result)) : ?>
-	<h4>Odsetki: <?= $result ?></h4>
+	echo $twig->render('credit/credit.twig', [
+		'amount' => $amount ?? '',
+		'years' => $years ?? '',
+		'interest' => $interest ?? '',
+		'result' => $result ?? null,
+		'menu' => $menu,
+	]);
 
-<?php endif; ?>
+} else {
+	?>
+	<!DOCTYPE HTML>
+	<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+	<head>
+	<meta charset="utf-8" />
+	<title>Kalkulator</title>
+	</head>
+	<body>
+		<?php require_once dirname(__FILE__).'/../../component/menu.php'; ?>
+	<form action="/app/views/credit/calc.php" method="post">
+		<label for="id_x">Kwota: </label>
+		<input id="id_x" type="number" name="amount" value="<?= $amount ?? '' ?>" /><br />
+		<label for="id_x">Lata: </label>
+		<input id="id_x" type="number" name="years" value="<?= $years ?? '' ?>" /><br />
 
-</body>
-</html>
+		<label for="id_op">Oprocentowanie: </label>
+		<input id="id_op" type="text" name="interest"
+			inputmode="decimal"
+			pattern="[0-9]+([.,][0-9]+)?"
+			title="Użyj przecinka lub kropki jako separatora dziesiętnego"
+			value="<?= htmlspecialchars($interest ?? '', ENT_QUOTES) ?>" /><br />
+		<input type="submit" value="Oblicz" />
+	</form>    
+
+	<?php if (isset($result)) : ?>
+		<h4>Odsetki: <?= $result ?></h4>
+
+	<?php endif; ?>
+
+	</body>
+	</html>
+	<?php
+}
--- config.php
+++ config.php
@@ -2,3 +2,22 @@
 define('_CALC_PATH', '/app/views/calc');
 define('_CREDIT_PATH', '/app/views/credit');
 define('_APP_URL', 'http://localhost:8080');
+
+// Optional: initialize Twig if Composer autoload is available
+$vendorAutoload = __DIR__ . '/vendor/autoload.php';
+if (file_exists($vendorAutoload)) {
+	require_once $vendorAutoload;
+
+	try {
+		$twigLoader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/app/views');
+		// disable cache for development; change to __DIR__.'/var/cache' for production
+		$twig = new \Twig\Environment($twigLoader, [
+			'cache' => false,
+		]);
+	} catch (Throwable $e) {
+		// If Twig classes are not found or other errors, skip initialization
+		$twig = null;
+	}
+} else {
+	$twig = null;
+}
